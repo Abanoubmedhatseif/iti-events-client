644eca7e97ae373a1ab6c3fc9d538828732f34e2
omniagad25 <omniagad25@gmail.com>
2024-06-25 11:37:51 +0300

    adding current events page

--- src/components/Event/EventCard.jsx
+++ src/components/Event/EventCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardMedia, Typography, CardActionArea } from '@mui/material';
+import { Card, CardContent, Typography, CardActionArea } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function EventCard({ id, name, description, startDate }) {
