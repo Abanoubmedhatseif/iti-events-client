d751eced2c565837bd7556ae7b37ccfad380d351
omniagad25 <omniagad25@gmail.com>
2024-06-25 18:16:31 +0300

    styling event-details page

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Container, CircularProgress, Typography, Grid, Card, CardContent, Box } from '@mui/material';
-import { fetchEventDetails, clearEvent } from '../store/Events/eventSlice'; // Adjust the path as needed
+import { fetchEventDetails, clearEvent } from '../store/Events/eventSlice';
+import Button from '../components/Button';
+import '../styles/EventDetails.css';
 
 function EventDetails() {
   const { eventId } = useParams();
@@ -19,7 +21,7 @@ function EventDetails() {
 
   if (loading) {
     return (
-      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" className="fade-in">
         <CircularProgress />
         <Typography variant="h6" component="div" style={{ marginLeft: '16px' }}>Loading...</Typography>
       </Box>
@@ -28,7 +30,7 @@ function EventDetails() {
 
   if (error) {
     return (
-      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" className="fade-in">
         <Typography variant="h6" component="div">Error: {error}</Typography>
       </Box>
     );
@@ -36,39 +38,65 @@ function EventDetails() {
 
   if (!event) {
     return (
-      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" className="fade-in">
         <Typography variant="h6" component="div">Event not found.</Typography>
       </Box>
     );
   }
 
+  const handleRegisterClick = () => {
+    // register for event
+    console.log('Register button clicked');
+  };
+
   return (
-    <Container maxWidth="md" style={{ marginTop: '20px' }}>
+    <Container maxWidth="md" style={{ marginTop: '20px' }} className="fade-in">
       <Card>
-        <CardContent>
-          <Typography gutterBottom variant="h4" component="div" align="center">
-            {event.name}
-          </Typography>
-          
-          
+        <CardContent className="slide-in-left card-content">
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12} md={4} className="centered">
+              <Typography gutterBottom variant="h4" component="div" align="center" style={{ fontSize: '2rem' }}>
+                {event.name}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={8} className="centered">
+              <Box>
+                <Typography variant="body1" color="textSecondary" align="center" style={{ fontSize: '1.5rem' }}>
+                  {event.description}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" align="center" style={{ fontSize: '1.25rem' }}>
+                  Start Date: {new Date(event.startDate).toLocaleString()}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" align="center" style={{ fontSize: '1.25rem' }}>
+                  End Date: {new Date(event.endDate).toLocaleString()}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" align="center" style={{ fontSize: '1.25rem' }}>
+                  Price: EGP{event.price}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" align="center" style={{ fontSize: '1.25rem' }}>
+                  Duration: {event.duration} {event.duration > 1 ? 'Hours' : 'Hour'}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" align="center" style={{ fontSize: '1.25rem' }}>
+                  Status: {event.registrationClosed ? 'Registration Closed' : 'Open To Register'}
+                </Typography>
+                {!event.registrationClosed && (
+                  <Box display="flex" justifyContent="center" marginTop="20px">
+                    <Button
+                      text="Register"
+                      backgroundColor="#3f51b5"
+                      color="#ffffff"
+                      width={200}
+                      height={50}
+                      fontSize={100}
+                      onClick={handleRegisterClick}
+                    />
+                  </Box>
+                )}
+              </Box>
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
-
-      <Typography variant="h5" component="div" style={{ marginTop: '20px' }} align="center">
-            {event.description}
-          </Typography>
-      <Typography variant="h5" component="div" style={{ marginTop: '20px' }} align="center">
-            End Date: {new Date(event.endDate).toLocaleString()}
-          </Typography>
-          <Typography variant="h5" component="div" style={{ marginTop: '20px' }} align="center">
-            Price: EGP{event.price}
-          </Typography>
-          <Typography variant="h5" component="div" style={{ marginTop: '20px' }} align="center">
-            Duration: {event.duration} minutes
-          </Typography>
-          <Typography variant="h5" component="div" style={{ marginTop: '20px' }} align="center">
-            Status: {event.registrationClosed ? 'Registration Closed' : 'Open To Register'}
-          </Typography>
     </Container>
   );
 }
